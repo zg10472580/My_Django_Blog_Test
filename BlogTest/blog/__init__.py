f4091a5e89ada6d90c5211e23f1e3649149ba793
老飞机 <59568455+zg10472580@users.noreply.github.com>
2021-11-07 01:34:19 +0800

    Delete BlogTest directory

--- BlogTest/blog/__init__.py
+++
@@ -1,6 +0,0 @@
-import platform
-
-osName = platform.system()
-if (osName == 'Linux'):
-    import pymysql
-    pymysql.install_as_MySQLdb()
--- BlogTest/blog/tool.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Time     :  2021/8/3 9:32
-# Author   :  老飞机
-# File     :  tool.py
-# Software :  PyCharm
-from django.shortcuts import render
-
-def tool_index(request):
-    return render(request, 'tool/index.html')
